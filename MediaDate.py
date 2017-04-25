847fe211d9dece7a4ed2f1035d55bd26bfcd72f2
Pedro Galan <pedro.galan@piksel.com>
2017-04-25 22:53:47 +0200

    Update conversion string to date to support new formats

--- MediaDate.py
+++ MediaDate.py
@@ -1,9 +1,13 @@
 from dateutil.parser import parse
+from datetime import datetime
 
 class MediaDate:
 
     def __init__(self, stringDate):
-        self.date = parse(self.__normaliseStringDate(stringDate))
+	# TODO Update the conversion string to date to make it robust
+        s = self.__normaliseStringDate(stringDate)
+	f = "%Y:%m:%d %H:%M:%S"
+	self.date = datetime.strptime(s, f)
 
     def toFileName(self):
         return self.date.strftime('%Y-%m-%d_%H.%M.%S')
