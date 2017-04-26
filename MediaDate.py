d88f9116be9e851b8782b8db199ad307123c1ae8
Pedro Galan <pedro.galan@piksel.com>
2017-04-26 06:48:28 +0100

    Update date parser to be more flexible

--- MediaDate.py
+++ MediaDate.py
@@ -2,21 +2,27 @@ from dateutil.parser import parse
 from datetime import datetime
 
 class MediaDate:
+    DATETIME_FORMAT = "%Y:%m:%d %H:%M:%S"
 
     def __init__(self, stringDate):
-	# TODO Update the conversion string to date to make it robust
-        s = self.__normaliseStringDate(stringDate)
-	f = "%Y:%m:%d %H:%M:%S"
-	self.date = datetime.strptime(s, f)
+        self.date = self.__parse(stringDate)
 
     def toFileName(self):
         return self.date.strftime('%Y-%m-%d_%H.%M.%S')
 
     def __normaliseStringDate(self, stringDate):
         if self.__hasTime(stringDate):
-            return stringDate
+            normalisedDate = stringDate
         else:
-            return stringDate + " 00:00:00Z"
+            normalisedDate = stringDate + " 00:00:00"
+        return self.__excludeTimeZoneInfo(normalisedDate)
+
+    def __excludeTimeZoneInfo(self, stringDate):
+        return stringDate[0:19]
 
     def __hasTime(self, stringDate):
         return " " in stringDate
+
+    def __parse(self, stringDate):
+        normalisedDate = self.__normaliseStringDate(stringDate)
+        return datetime.strptime(normalisedDate, self.DATETIME_FORMAT)
