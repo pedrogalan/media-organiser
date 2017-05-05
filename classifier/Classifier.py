63a6235c2a08656a8d6c956ade9d7607c2c3f93b
Pedro Galan <pedro.galan@piksel.com>
2017-05-05 12:00:13 +0100

    Implement the classifier.

--- classifier/Classifier.py
+++ classifier/Classifier.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 sys.path.append('../config')
 sys.path.append('../log')
@@ -5,7 +6,7 @@ sys.path.append('../entities')
 sys.path.append('../utils')
 from config.Config import Config
 from log.Logging import logging
-from entities.Media import Media
+from entities.File import File
 from utils.FileUtils import FileUtils
 from glob import glob
 
@@ -17,4 +18,26 @@ class Classifier:
         self.destinationPath = Config.get('classifier.path.destination')
 
     def run(self):
-        print 'Running classifier.'
+        for filename in self.__getFilenamesToClassify():
+            try:
+                self.__classify(filename)
+            except:
+                self.__handleError(filename)
+
+    def __getFilenamesToClassify(self):
+        extensions = tuple(Config.get('classifier.path.sources.file.extensions').split(','))
+        return FileUtils.findFilesRecursivelly(self.sourcePath, extensions, Config.get('classifier.max.number.of.files'))
+
+    def __classify(self, filename):
+        file = File(filename)
+        destinationFilename = self.__buildDestinationFilename(file, self.destinationPath)
+        FileUtils.move(file.fullPath, destinationFilename)
+
+    def __buildDestinationFilename(self, file, destinationPath):
+        return os.path.join(destinationPath, file.partialPath, file.name)
+
+    def __handleError(self, filename):
+        self.numberOfErrors = self.numberOfErrors + 1
+        logging.error('Error classifying %s. Error number %s.', str(filename), str(self.numberOfErrors), exc_info=True)
+        if self.numberOfErrors == int(Config.get('classifier.max.number.of.errors')):
+            raise ValueError('Too many errors. Something is wrong. Aborting execution.')
--- shrinker/UnknownShrinker.py
+++ shrinker/UnknownShrinker.py
@@ -7,8 +7,7 @@ class UnknownShrinker:
         self.destinationFilename = self.__buildDestinationFilename(file, destinationPath)
 
     def shrink(self):
-        # TODO
-        print('Shrinking unknown format:\nFrom: ' + self.sourceFilename + '\nTo  : ' + self.destinationFilename)
+        FileUtils.move(self.sourceFilename, self.destinationFilename)
 
     def __buildDestinationFilename(self, file, destinationPath):
         return os.path.join(destinationPath, file.partialPath, file.name)
