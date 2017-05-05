7a43f640f3471abb7d0a23ca2baaa532848d516d
Pedro Galan <pedro.galan@piksel.com>
2017-05-05 11:59:23 +0100

    Make 'createDestinationDirectory()' public to be used by the classifier

--- utils/FileUtils.py
+++ utils/FileUtils.py
@@ -4,6 +4,7 @@ from os.path import join
 from os.path import exists
 from os.path import dirname
 from os import walk
+from os import remove
 from os import makedirs
 from shutil import copyfile
 from shutil import move
@@ -23,19 +24,32 @@ class FileUtils:
 
     @staticmethod
     def copy(sourceFilename, destinationFilename):
-        FileUtils.__createDestinationDirectory(destinationFilename)
+        FileUtils.createDestinationDirectory(destinationFilename)
         copyfile(sourceFilename, destinationFilename)
 
     @staticmethod
     def move(sourceFilename, destinationFilename):
-        FileUtils.__createDestinationDirectory(destinationFilename)
-        copyfile(sourceFilename, destinationFilename)
+        FileUtils.createDestinationDirectory(destinationFilename)
+        move(sourceFilename, destinationFilename)
 
     @staticmethod
     def rename(media, destinationPath):
         destinationFile = FileUtils.__getDestinationFilename(destinationPath, media)
-        FileUtils.__createDestinationDirectory(destinationFile)
+        FileUtils.createDestinationDirectory(destinationFile)
         move(media.sourceFile, destinationFile)
+
+    @staticmethod
+    def delete(filePath):
+        remove(filePath)
+
+    @staticmethod
+    def createDestinationDirectory(filename):
+        if not exists(dirname(filename)):
+            try:
+                makedirs(dirname(filename))
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise
 
     @staticmethod
     def __getDestinationFilename(destinationPath, media):
@@ -54,12 +68,3 @@ class FileUtils:
             return join('Videos', subdir)
         else:
             return join('Unknown', subdir)
-
-    @staticmethod
-    def __createDestinationDirectory(filename):
-        if not exists(dirname(filename)):
-            try:
-                makedirs(dirname(filename))
-            except OSError as exc:
-                if exc.errno != errno.EEXIST:
-                    raise
