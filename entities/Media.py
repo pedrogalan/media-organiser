54cd9509e9293324bc1d0aa1f9f9c56e741fb33b
Pedro Galan <pedro.galan@piksel.com>
2018-04-29 21:52:03 +0100

    Rename to "creation date" which is clearer

--- entities/Media.py
+++ entities/Media.py
@@ -1,12 +1,15 @@
 class Media:
 
-    def __init__(self, origin, fullPath, fileext, mediaName, mediaType, mediaDate):
+    def __init__(self, origin, fullPath, fileext, mediaName, mediaType, creationDate):
         self.origin = origin
         self.fullPath = fullPath
         self.fileext = fileext
         self.mediaType = mediaType
         self.mediaName = mediaName
-        self.mediaDate = mediaDate
+        self.creationDate = creationDate
+
+    def getFullPath(self):
+        return self.fullPath
 
     def getNewFileName(self):
         return self.mediaName.getName() + '.' + self.fileext
@@ -23,12 +26,15 @@ class Media:
     def getCreationYear(self):
         return self.mediaName.getName()[0:4]
 
+    def getCreationDate(self):
+        return self.creationDate
+
     def __str__(self):
         return 'Path: ' + self.fullPath \
             + '\nOrigin: ' + self.origin \
             + '\nFileext: ' + self.fileext \
             + '\nMedia type: ' + self.mediaType \
             + '\nCreation year: ' + self.getCreationYear() \
-            + '\nMedia date: ' + self.mediaDate.toFileName() \
+            + '\nCreation date: ' + str(self.creationDate) \
             + '\nMedia name: ' + self.getNewFileName() \
             + '\n'
